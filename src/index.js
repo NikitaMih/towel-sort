96e15ce0f095ae2a86fb08df7f0da34d31cde858
NikitaMih <70882448+NikitaMih@users.noreply.github.com>
2021-03-14 23:15:21 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    for (let j=0; j<matrix.length; j++){
+        for (let i=0; i<matrix.length - 1; i++){
+            if (matrix[i] > matrix[i+1]) {
+                const buff = matrix[i]
+                matrix[i] = matrix[i+1]
+                matrix[i+1] = buff
+            }
+        }
+    }
 }
