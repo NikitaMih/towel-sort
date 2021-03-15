4b8b54319467a4efc393b9a7bd24aed6e473b06e
NikitaMih <70882448+NikitaMih@users.noreply.github.com>
2021-03-15 21:57:43 +0300

    new

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function towelSort (matrix) {
 matrix.flat();
     for (let j=0; j<matrix.length; j++){
         for (let i=0; i<matrix.length - 1; i++){
-            if (array[i] > matrix[i+1]) {
+            if (matrix[i] > matrix[i+1]) {
                 const buff = matrix[i]
                 matrix[i] = matrix[i+1]
                 matrix[i+1] = buff
